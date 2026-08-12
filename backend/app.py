20c1e03084de8ae007403257587c2713b94a8c95
Maxessien <essienmax484@gmail.com>
2026-08-12 02:38:07 +0100

    Refactor get_vid_formats() by removing unnecessary HTTP headers

--- backend/app.py
+++ backend/app.py
@@ -134,12 +134,7 @@ def get_vid_formats():
                 "youtube": {
                     "player_client": ["ios", "android"],
                 }
-            },
-            "http_headers": {
-                "User-Agent": "com.google.ios.youtube/19.29.1 (iPhone16,2; U; CPU iPhone OS 17_5_1 like Mac OS X; en_US)",
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.9",
-            },
+            }
         }
 
         yt = YoutubeDL(opt)
