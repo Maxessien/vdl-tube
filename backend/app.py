2502d5eff26b4ef3046c3e37a3d4c98ca6558623
Max Essien <essienmax484@gmail.com>
2026-08-16 09:32:49 +0100

    Enable cookiefile option in YoutubeDL configuration

--- backend/app.py
+++ backend/app.py
@@ -126,7 +126,7 @@ def get_vid_formats():
         opt = {
             "no_warnings": True,
             "retries": 5,
-            #"cookiefile": build_cookie_file(),
+            "cookiefile": build_cookie_file(),
             "skip_download": True,
         }
 
